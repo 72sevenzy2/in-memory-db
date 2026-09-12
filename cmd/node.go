3ce6be94f97269573378479e0054cfd8bf70ed42
72 <72sevenzy2@gmail.com>
2026-09-12 14:08:25 +0500

    feat: add node role encapsulations, and constructor for Node{}.

--- cmd/node.go
+++ cmd/node.go
@@ -1,25 +1,39 @@
 package cmd
 
 import (
+	"sync"
+
 	"github.com/72sevenzy2/in-memory-database/db"
 )
 
 // a node represents an database instance.
 
+type Role uint8 // small integer which dictates a nodes role.
+
 type Node struct {
+	lock sync.RWMutex
+
 	ID   string
 	Addr string
+	role Role
 
-	// Replicas represent a string array of existing nodes ports.
+	// Replicas represent a string array of existing nodes addresses.
 	Replicas []string
 
 	DB *db.DB
 }
-
-type Role uint8
 
 // Represents a nodes role, either an follower or leader.
 const (
 	Leader Role = iota
 	Follower
 )
+
+func NewNode(id, addr string, role Role) *Node {
+	return &Node{
+		ID:   id,
+		Addr: addr,
+		role: role,
+		DB:   db.NewDB(),
+	}
+}
